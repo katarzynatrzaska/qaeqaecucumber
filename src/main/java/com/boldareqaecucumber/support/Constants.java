ab3d135e2cdf4e1f26ee5ae3fac26abd7011c24a
katarzynatrzaska <42570868+katarzynatrzaska@users.noreply.github.com>
2020-03-26 16:50:37 +0100

    Update Constants.java

--- src/main/java/com/boldareqaecucumber/support/Constants.java
+++ src/main/java/com/boldareqaecucumber/support/Constants.java
@@ -3,9 +3,6 @@ package com.boldareqaecucumber.support;
 
 public class Constants {
 
-
-	public final static String userName = "admin";
-	public final static String password = "password";
 	
 	public final static long explicitWait = 100;
 	public final static long impliciteWait = 100;
